882401dc6f14ade9f5a6ed624cc091e46148b208
miyazaki <miyazaki424@outlook.jp>
2023-07-29 16:36:56 +0900

    基本構成

---
+++ lib/pages/miyazaki.dart
@@ -0,0 +1,13 @@
+import 'package:chains_game/main.dart';
+import 'package:flutter/material.dart';
+
+@override
+class MiyazakiPage extends State<MyHomePage> {
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Miyazaki"),
+        ),
+        body: Center());
+  }
+}
---
+++ lib/pages/yamamoto.dart
@@ -0,0 +1,13 @@
+import 'package:chains_game/main.dart';
+import 'package:flutter/material.dart';
+
+@override
+class YamamotoPage extends State<MyHomePage> {
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Yamamoto"),
+        ),
+        body: Center());
+  }
+}
